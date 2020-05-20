42f711122fc670a1731e56f20438cee2c1548f86
Hasib Ahmed <hasibahmed1996@gmail.com>
2020-05-20 19:20:43 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=18KgHUP-NMhhN1bpFRpx8H_7WDX8q2kem'
+export_file_name = 'skin_lesion_image_classifier_model_wtl.sav'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
